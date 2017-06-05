69d5d155e10917d891ec039b6491fd8e9b03b2bd
encadyma <encadyma@gmail.com>
2017-06-05 10:48:25 -0700

    Implemented basic routing pt.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LaunchComponent } from './launch.component';
 import { AuthComponent } from './auth.component';
+
+const appRoutes: Routes = [
+  { path: 'auth', component: AuthComponent },
+  { path: 'launch', component: LaunchComponent },
+  { path: '', redirectTo: '/launch', pathMatch: 'full' },
+  { path: '**', component: LaunchComponent }  // TODO: need to change to 404
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +24,8 @@ import { AuthComponent } from './auth.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
