3289691f6a222df8b4eb8aa2eecb040d2d3e686b
encadyma <encadyma@gmail.com>
2017-06-05 09:56:35 -0700

    Moved html files to separate html folder.

--- src/app/launch.component.ts
+++ src/app/launch.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-launch',
-  templateUrl: './launch.component.html',
+  templateUrl: './html/launch.component.html',
   styleUrls: ['./css/launch.component.css']
 })
 export class LaunchComponent {
