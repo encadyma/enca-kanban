ea1a296156a85570d6b702b3457af804cf9dc7f8
encadyma <encadyma@gmail.com>
2017-06-05 10:26:32 -0700

    Added authentication component placeholder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LaunchComponent } from './launch.component';
+import { AuthComponent } from './auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LaunchComponent
+    LaunchComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './html/auth.component.html',
+  styleUrls: ['./css/auth.component.css']
+})
+export class AuthComponent {
+
+}
