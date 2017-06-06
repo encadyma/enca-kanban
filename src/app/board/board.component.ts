9846722d6bed59e692191618d419dcf66da05837
encadyma <encadyma@gmail.com>
2017-06-05 19:23:23 -0700

    Added sample deck to mock data obj., added label support in card class

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -16,34 +16,43 @@ export class BoardComponent {
       {
         title: "Welcome to Project Kanban!",
         cards: [
-          { title: "Project Kanban Rules and Announcements" },
-          { title: "Did you know?" },
-          { title: "Something colorful to spice up things!" },
-          { title: "Not a very good example of a one-liner card, huh?" }
+          { title: "Project Kanban Rules and Announcements", labels: ['red', 'orange', 'yellow', 'turq'] },
+          { title: "Did you know?", labels: ['red', 'orange', 'yellow', 'turq'] },
+          { title: "Something colorful to spice up things!", labels: ['red', 'orange', 'yellow', 'turq'] },
+          { title: "Not a very good example of a one-liner card, huh?", labels: ['red', 'orange', 'yellow', 'turq'] }
         ]
       },
       {
         title: "Board Rules",
         cards: [
-          { title: "Rule #1: Never talk about the board club!" },
-          { title: "Rule #2: Keep your hands clean at all times." },
-          { title: "Rule #3: Make sure you're always living it up." },
-          { title: "Rule #4: There are no more rules!" },
+          { title: "Rule #1: Never talk about the board club!", labels: ['red', 'orange', 'yellow', 'turq'] },
+          { title: "Rule #2: Keep your hands clean at all times.", labels: ['red', 'orange', 'yellow', 'turq'] },
+          { title: "Rule #3: Make sure you're always living it up.", labels: ['red', 'orange', 'yellow', 'turq'] },
+          { title: "Rule #4: There are no more rules!", labels: ['red', 'orange', 'yellow', 'turq'] },
         ]
       },
       {
         title: "List of Todos",
         subtitle: "Linked to PhraseFactory, last updated on 8:15 PM",
         cards: [
-          { title: "Clean up the dishes daily" },
-          { title: "Set up an appointment with Jaine by 6/17" }
+          { title: "Clean up the dishes daily", labels: ['red', 'orange', 'yellow', 'turq'] },
+          { title: "Set up an appointment with Jaine by 6/17", labels: ['red', 'orange', 'yellow', 'turq'] }
         ]
       },
       {
         title: "Done",
         cards: [
-          { title: "1-hour dubstep mix for College Festa" },
-          { title: "50 page paper on AI (due by finals)" }
+          { title: "1-hour dubstep mix for College Festa", labels: ['red', 'orange', 'yellow', 'turq'] },
+          { title: "50 page paper on AI (due by finals)", labels: ['red', 'orange', 'yellow', 'turq'] }
+        ]
+      },
+      {
+        title: "Sample Deck",
+        subtitle: "Subtitle to put integration info",
+        cards: [
+          { title: "Card Test #1", labels: ['orange', 'turq'] },
+          { title: "Card w/ no labels!"},
+          { title: "Now a card with all default labels! (special, isn't it?)", labels: ['red', 'orange', 'yellow', 'green', 'turq', 'blue', 'purple', 'black'] }
         ]
       }
     ]
--- src/app/board/deck-card.component.ts
+++ src/app/board/deck-card.component.ts
@@ -4,11 +4,8 @@ import { Component, Input } from '@angular/core';
   selector: 'deck-card',
   template: `
   <div class="deck-card" [class.active]="card === selectedCard">
-    <ul class="deck-card-labels">
-      <li class="deck-card-label red"></li>
-      <li class="deck-card-label orange"></li>
-      <li class="deck-card-label yellow"></li>
-      <li class="deck-card-label turq"></li>
+    <ul class="deck-card-labels" *ngIf="card.labels">
+      <li class="deck-card-label" [ngClass]="['deck-card-label', label]" *ngFor="let label of card.labels"></li>
     </ul>
     <span class="deck-card-title">{{card.title}}</span>
   </div>`,
