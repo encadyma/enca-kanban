78bb5b84a4c6fbe736d7eb1571b433007010ab0a
encadyma <encadyma@gmail.com>
2017-06-07 17:23:48 -0700

    Comments on settings service

--- src/app/settings/settings.service.ts
+++ src/app/settings/settings.service.ts
@@ -3,11 +3,12 @@ import { Injectable } from '@angular/core';
 import { SettingsArray } from './settings'
 import { MOCK_SETTINGS } from './mock-settings';
 
+// SettingService functions as a data interface for settings.
+
 @Injectable()
 export class SettingsService {
-
+  // Returns settings array via Promise
   getSettings(): Promise<SettingsArray> {
     return Promise.resolve(MOCK_SETTINGS);
   }
-
 }
